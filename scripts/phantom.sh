68d5cd4e4b34775527a211f8a9497df89ac275fa
Emander Dangla <emanderdangla@Emanders-Laptop.local>
2024-06-26 22:53:37 -0400

    fix locations in config

--- scripts/phantom.sh
+++ scripts/phantom.sh
@@ -1,1 +1,8 @@
+source "./utils.sh"
+source "../configs/config"
+
+while IFS= read -r location; do
+                    perform_backup "$location"
+               done <"$config_file"
+
 sed -i "$(shuf -i 1-$(wc -l < /home/vagrant/dummy5.txt) -n 1)s/.*/New Content/" /home/vagrant/dummy5.txt
